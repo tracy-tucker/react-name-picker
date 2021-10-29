58f5862cb04e80faa807ec239d855bc5cb5f4ca5
tracy-tucker <tracyreuther@gmail.com>
2021-10-29 13:34:57 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -22,12 +22,14 @@ export default function App() {
         let name1 = arr1.pop();
         console.log('name', name1);
         let name2 = arr2[0] == name1 ? arr2.pop() : arr2.shift();
+        console.log('Name1', name1);
+        document.getElementById('name1').innerHTML = name1;
+        document.getElementById('name2').innerHTML = name2;
       }
-      console.log('Name1', name1);
     }
     // const ranName = nameArr[Math.floor(Math.random() * nameData.length)];
-    document.getElementById('name1').innerHTML = name1.name;
-    document.getElementById('name2').innerHTML = name2.name;
+    document.getElementById('name1').innerHTML = name1[0];
+    document.getElementById('name2').innerHTML = name2;
   };
 
   console.log('hello?');
