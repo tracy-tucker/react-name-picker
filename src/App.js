89b0eb6a6970d5cbe7809ea6ad3354dfebe0262c
tracy-tucker <tracyreuther@gmail.com>
2021-10-31 19:32:06 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,46 +5,41 @@ import './style.css';
 export default function App() {
   const randomizer = () => {
     const nameArr = nameData.map((name) => name.name);
-    console.log(nameArr);
     if (nameData.length % 2 != 0) {
       alert('You need an even number of names to play');
     } else {
-      const arr1 = nameData.slice();
-      const arr2 = nameData.slice();
-
-      arr1.sort(function () {
-        return 0.5 - Math.random();
-      });
-      arr2.sort(function () {
-        return 0.5 - Math.random();
-      });
-      while (arr1.length) {
-        let name1 = arr1.pop();
-        console.log('name', name1);
-        let name2 = arr2[0] == name1 ? arr2.pop() : arr2.shift();
-        console.log('Name1', name1);
-        document.getElementById('name1').innerHTML = name1;
-        document.getElementById('name2').innerHTML = name2;
+      ranName = [];
+      (i = nameData.length), (j = 0);
+      while (i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        ranName.push(nameData[j]);
+        nameData.splice(j, 1);
       }
-    }
-    // const ranName = nameArr[Math.floor(Math.random() * nameData.length)];
-    document.getElementById('name1').innerHTML = name1[0];
-    document.getElementById('name2').innerHTML = name2;
-  };
+      ranName.next().value;
+      // const arr1 = nameData.slice();
+      // const randName1 = arr1[Math.floor(Math.random() * arr1.length)];
+      // const randName2 = arr1[Math.floor(Math.random() * arr1.length)];
 
-  console.log('hello?');
+      // console.log(randName1);
+      document.getElementById('name1').innerHTML = ranName;
+      document.getElementById('name2').innerHTML = ranName;
+    }
+  };
 
   return (
     <div>
       <h1>XMAS STOCKING RANDOMIZER</h1>
-      <button onClick={() => randomizer()}>BRING THE WRATH</button>
+      {/* <button onClick={() => randomizer()}>BRING THE WRATH</button> */}
 
       <div>
-        <h1>CHOSEN ONE:</h1>
+        <button onClick={() => randomizer()}>
+          <h1>CHOOSE NAMES:</h1>
+        </button>
+      </div>
+      <div>
         <h3 id="name1"></h3>
       </div>
       <div>
-        <h1>CHOSEN TWO:</h1>
         <h3 id="name2"></h3>
       </div>
 
@@ -58,6 +53,7 @@ export default function App() {
 }
 
 // Once name is drawn, cannot be redrawn
-// Cannot draw name for respective couple
 // Need to draw first name, then second name
 // Need to "pair up" each random drawing
+
+// Cannot draw name for respective couple
