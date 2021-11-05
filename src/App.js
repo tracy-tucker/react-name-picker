3a0e853e3530bac0693273e141d48695a5e7a67e
tracy-tucker <tracyreuther@gmail.com>
2021-11-05 15:54:07 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,31 +5,23 @@ import './style.css';
 export default function App() {
   const randomizer = () => {
     const nameArr = nameData.map((name) => name.name);
-    if (nameData.length % 2 != 0) {
-      alert('You need an even number of names to play');
-    } else {
-      ranName = [];
-      (i = nameData.length), (j = 0);
-      while (i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        ranName.push(nameData[j]);
-        nameData.splice(j, 1);
-      }
-      ranName.next().value;
-      // const arr1 = nameData.slice();
-      // const randName1 = arr1[Math.floor(Math.random() * arr1.length)];
-      // const randName2 = arr1[Math.floor(Math.random() * arr1.length)];
+    const n = nameArr[Math.floor(Math.random() * nameArr.length)];
+    console.log(n);
+    document.getElementById('name1').innerHTML = n;
+    const index = nameArr.indexOf(n);
+    // console.log(index);
+    nameData.splice(index, 1);
 
-      // console.log(randName1);
-      document.getElementById('name1').innerHTML = ranName;
-      document.getElementById('name2').innerHTML = ranName;
-    }
+    console.log('NAME', nameArr);
+
+    // if (n == nameData.name) {
+    //   //
+    // }
   };
 
   return (
     <div>
       <h1>XMAS STOCKING RANDOMIZER</h1>
-      {/* <button onClick={() => randomizer()}>BRING THE WRATH</button> */}
 
       <div>
         <button onClick={() => randomizer()}>
@@ -38,9 +30,6 @@ export default function App() {
       </div>
       <div>
         <h3 id="name1"></h3>
-      </div>
-      <div>
-        <h3 id="name2"></h3>
       </div>
 
       <ul>
